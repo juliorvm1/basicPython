b14a136208f331f95f9792b037e8d4a8ec345c98
juliorvm1 <juliorvm1@gmail.com>
2021-10-28 15:05:58 -0500

    se agrego el programa recorrer cadena.py

---
+++ recorrercadena.py
@@ -0,0 +1,11 @@
+def run():
+    nombre ="Julio Ricardo Vicente Martínez"
+    for i in nombre:
+        print(i)
+    
+    for caracter in nombre:
+        print (caracter.upper())
+
+
+if __name__=="__main__":
+    run()
